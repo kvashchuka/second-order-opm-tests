9b80b57c5e66124341ef74bce9940f6f36620524
Anna Kvashchuk <kvashchuk.anna@gmail.com>
2023-03-23 22:33:49 +0100

    Add nessesary files for running CO2 injection scenario on Norne field with three different well configureations

---
+++ norne_co2/testing_NorneCO2_behind_fault.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ../../../../OPM_2020.04-pre/opm-simulators/build/
+make flow
+cd ../../../tests_2020.04-pre/norne/norne_refined/e100
+
+EXEC=~/OPM_withtests/OPM_2020.04-pre/opm-simulators/build/bin/flow
+
+CASEX1_CO2=X1_SIMPLE_CO2_FAULT
+
+TIMESTEP=1
+
+cd X1
+
+$EXEC $CASEX1_CO2 --output-dir=1st_difwells100_$CASEX1_CO2 &> 1st_difwells100_$CASEX1_CO2.txt
+
+$EXEC $CASEX1_CO2  --enable-higher-order=1 --enable-local-reconstruction=1 --reconstruction-scheme-id=3 --only-reconstruction-for-solvent-or-polymer=false --output-dir=LP_notonly_difwells100_$CASEX1_CO2 &> LP_notonly_difwells100_$CASEX1_CO2.txt
+
